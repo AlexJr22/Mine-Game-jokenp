9b542ac83a0d3814c1ed2d6d24084dfa603fe301
Alex Junior <alexjr2oficial@gmail.com>
2022-07-14 13:35:26 -0300

    sistema de escolhas

--- jokenpô/script.js
+++ jokenpô/script.js
@@ -1,8 +1,52 @@
-function jogar(n){
-    let compt = (function(){
+const respComputador = document.getElementById("Rpc")
+
+
+function jogar(String){
+    const items = (() => {
         const random = (min, max) => Math.floor(Math.random() * (max - min) + min)
-        const items = ['Pedra', 'Papel', 'Tesoura']
-        return items[random(0, items.length)]
+        const choice = ['pedra', 'papel', 'tesoura']
+        return choice[random(0, 3)]
     })()
     
+    if(String == 'pedra') { // Se o usuário escolher PEDRA
+        
+        if(items == 'pedra') { // EMPATE
+            // console.log('pedra')
+            
+        }
+        if(items == 'papel') { // DERROTA
+            console.log('papel')
+        }
+        if(items == 'tesoura') { // VITORIA
+            console.log('tesoura')
+        }
+
+    }
+    if(String == 'papel') { // se o usuário escolher PAPEL
+
+        if(items == 'pedra') { // VITORIA
+            console.log('pedra')
+        }
+        if(items == 'papel') { // EMPATE
+            console.log('papel')
+        }
+        if(items == 'tesoura') { // DERROTA
+            console.log('tesoura')
+        }
+        
+    }
+    if(String == 'tesoura') { //se o usuário escolher TESOURA
+        
+        if(items == 'pedra') { // DERROTA
+            console.log('pedra')
+        }
+        if(items == 'papel') {  // VITORIA
+            console.log('papel')
+        }
+        if(items == 'tesoura') { // EMPATE
+            console.log('tesoura')
+        }
+        
+    }
 }
+
